4d88ca110435512163d0558eb6fe3dd1d8ba0de3
Levent Saatci <leventsaatci@gmail.com>
2019-02-15 21:23:24 +0100

    some minor chages

--- CoinGecko/Entities/Response/Exchanges/ExchangeById.cs
+++ CoinGecko/Entities/Response/Exchanges/ExchangeById.cs
@@ -1,26 +1,15 @@
+using System;
 using CoinGecko.Entities.Response.Shared;
 using Newtonsoft.Json;
 
 namespace CoinGecko.Entities.Response.Exchanges
 {
-    public class ExchangeById:ExchangesMain
+    public class ExchangeById:Exchanges
     {
         [JsonProperty("tickers")]
         public Ticker[] Tickers { get; set; }
 
         [JsonProperty("status_updates")]
         public object[] StatusUpdates { get; set; }
-    }
-    
-    public class Market
-    {
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("identifier")]
-        public string Identifier { get; set; }
-
-        [JsonProperty("has_trading_incentive")]
-        public bool HasTradingIncentive { get; set; }
     }
 }

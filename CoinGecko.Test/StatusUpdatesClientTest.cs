76dc8d2a09fe070b4bd8a20af33dc85bc46733db
Levent Saatçi <leventsaatci@hotmail.com>
2020-07-07 12:40:07 +0200

    StatusUpdates tests added

--- CoinGecko.Test/StatusUpdatesClientTest.cs
+++ CoinGecko.Test/StatusUpdatesClientTest.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using CoinGecko.Clients;
 using CoinGecko.Interfaces;
+using CoinGecko.Parameters;
 using Xunit;
 
 namespace CoinGecko.Test
@@ -10,12 +12,24 @@ namespace CoinGecko.Test
     public class StatusUpdatesClientTest
     {
         private readonly ICoinGeckoClient _client;
-
+        
         public StatusUpdatesClientTest()
         {
             _client = CoinGeckoClient.Instance;
         }
 
+        [Theory]
+        [InlineData("general")]
+        [InlineData("milestone")]
+        public async Task Return_Category_Count_Must_be_Equal_to_All_count(string category)
+        {
+            
+                var result = await _client.StatusUpdatesClient.GetStatusUpdate(category, "", 100, 1);
+                var returnProjectCount = result.StatusUpdates.Count(x => x.Category == category);
+                var returnCount = result.StatusUpdates.Length;
+                Assert.Equal(returnCount,returnProjectCount);
+        }
+        
         [Theory]
         [InlineData("Market")]
         [InlineData("Coin")]

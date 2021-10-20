d74fb4d6ed0e26994ac7c1e0b8444ccb0f60b708
Levent Saatci <leventsaatci@gmail.com>
2021-10-20 12:50:03 +0200

    Missing properties added

--- CoinGecko/Entities/Response/Exchanges/ExchangesMain.cs
+++ CoinGecko/Entities/Response/Exchanges/ExchangesMain.cs
@@ -10,7 +10,16 @@ namespace CoinGecko.Entities.Response.Exchanges
         [JsonProperty("has_trading_incentive")]
         public bool? HasTradingIncentive { get; set; }
 
+        [JsonProperty("trust_score")]
+        public double? TrustScore { get; set; }
+        
+        [JsonProperty("trust_score_rank")]
+        public double? TrustScoreRank { get; set; }
+
         [JsonProperty("trade_volume_24h_btc")]
         public double? TradeVolume24HBtc { get; set; }
+        
+        [JsonProperty("trade_volume_24h_btc_normalized")]
+        public double? TradeVolume24HBtcNormalized { get; set; }
     }
 }

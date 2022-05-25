ae16f071dfdda25e815a378576eaa458b5aa5a40
whyme0 <sparkjetstudiost@gmail.com>
2022-05-25 21:53:10 +0300

    Implement get method

---
+++ CoinGecko/Clients/SearchClient.cs
@@ -0,0 +1,25 @@
+﻿using CoinGecko.Entities.Response.Search;
+using System.Collections.Generic;
+using CoinGecko.ApiEndPoints;
+using System.Threading.Tasks;
+using CoinGecko.Interfaces;
+using CoinGecko.Services;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace CoinGecko.Clients
+{
+    public class SearchClient : BaseApiClient, ISearchClient
+    {
+        public SearchClient(HttpClient httpClient, JsonSerializerSettings jsonSerializerSetting) : base(httpClient, jsonSerializerSetting)
+        {
+        }
+
+        public async Task<TrendingList> GetSearchTrending()
+        {
+            return await GetAsync<TrendingList>(
+                QueryStringService.AppendQueryString(SearchApiEndpoints.SearchTrending))
+                .ConfigureAwait(false);
+        }
+    }
+}

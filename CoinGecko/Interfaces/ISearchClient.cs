ed9146151d4ee882ccf574b86ce4119b038f2963
whyme0 <sparkjetstudiost@gmail.com>
2022-05-25 21:52:58 +0300

    Create get method

---
+++ CoinGecko/Interfaces/ISearchClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using CoinGecko.Entities.Response.Search;
+
+namespace CoinGecko.Interfaces
+{
+    public interface ISearchClient
+    {
+        /// <summary>
+        /// Get trending search coins (Top-7) on CoinGecko in the last 24 hours
+        /// </summary>
+        /// <returns></returns>
+        Task<TrendingList> GetSearchTrending();
+    }
+}

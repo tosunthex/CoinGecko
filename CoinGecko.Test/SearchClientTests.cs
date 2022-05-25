753126aa27651a9490331216861e1c3b1145234a
Levent <leventsaatci@hotmail.com>
2022-05-25 21:28:44 +0200

    Update SearchClientTests.cs
    reduced Assertion and removed Exchanged test for data which could my come later

--- CoinGecko.Test/SearchClientTests.cs
+++ CoinGecko.Test/SearchClientTests.cs
@@ -15,23 +15,10 @@ namespace CoinGecko.Test
         }
 
         [Fact]
-        public async Task SearchTrending_Exchanges_Empty()
-        {
-            var result = await _client.SearchClient.GetSearchTrending();
-            Assert.Empty(result.Exchanges);
-        }
-
-        [Fact]
         public async Task SearchTrending_TrendingItems_Fields_Not_Null()
         {
             var result = (await _client.SearchClient.GetSearchTrending()).TrendingItems[0].TrendingItem;
             Assert.NotNull(result.Id);
-            Assert.NotNull(result.Name);
-            Assert.NotNull(result.Symbol);
-            Assert.NotNull(result.Thumb);
-            Assert.NotNull(result.Small);
-            Assert.NotNull(result.Large);
-            Assert.NotNull(result.Slug);
         }
     }
 }

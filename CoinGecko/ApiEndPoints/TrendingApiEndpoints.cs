26703ae8ff053a7e365bcd9f1707301ad0cc3919
Levent <leventsaatci@hotmail.com>
2022-05-25 21:17:22 +0200

    Update TrendingApiEndpoints.cs
    static added

--- CoinGecko/ApiEndPoints/TrendingApiEndpoints.cs
+++ CoinGecko/ApiEndPoints/TrendingApiEndpoints.cs
@@ -1,6 +1,6 @@
 ﻿namespace CoinGecko.ApiEndPoints
 {
-    public class SearchApiEndpoints
+    public static class SearchApiEndpoints
     {
         public static readonly string SearchTrending = "search/trending";
     }
